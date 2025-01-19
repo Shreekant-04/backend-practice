708896812edf50a06ec751552bafa5599b52866b
Shreekant <shreekant4062@gmail.com>
2025-01-19 23:57:14 +0530

    `Added Stripe webhook handler and updated booking controller`

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 // const Email = require('./utils/email');
 
 const app = express();
@@ -41,6 +42,12 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use('/api', limiter);
+
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
